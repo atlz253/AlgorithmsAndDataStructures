ce63c03c70cbdd23645e9985f73e9d49abc93233
Fedor <f.alexeeff2010@yandex.ru>
2021-04-22 00:42:02 +0300

    нули на главной диагонали

--- lab_2/randomMatrix.py
+++ lab_2/randomMatrix.py
@@ -4,5 +4,8 @@ n = int(input('Введите размер матрицы: '))
 
 for i in range(n):
     for j in range(n):
-        print(str(randint(0, 1)), end=' ')
+        if i == j:
+            print('0', end=' ')
+        else:
+            print(str(randint(0, 1)), end=' ')
     print()
